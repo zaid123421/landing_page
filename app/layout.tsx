5242c6a01ab23dcbc8fb4fd84ede18597d50069c
Zaid Alshamaa <zshamaa20@gmail.com>
2025-10-29 16:07:37 +0300

    add meta tag for screen size

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body lang="en" className={raleway.className}>
         {children}
       </body>
